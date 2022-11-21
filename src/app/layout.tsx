84fe3d3f40124086e39e08878282596bec64d893
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-11-21 12:50:15 -0300

    chore: setup tailwindcss with app colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { PropsWithChildren } from 'react';
 
+import '../styles/global.css';
+
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="pt-BR">
