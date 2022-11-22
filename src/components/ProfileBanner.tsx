2310c09f2efcbb6a78db33467cab05ce18ceb3bd
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-11-21 21:22:34 -0300

    style: make responsive

--- src/components/ProfileBanner.tsx
+++ src/components/ProfileBanner.tsx
@@ -13,7 +13,7 @@ async function ProfileBannerBase() {
   const profile = await getGitHubProfile('eliasgcf');
 
   return (
-    <div className="flex rounded-[0.625rem] bg-base-profile drop-shadow-lg">
+    <div className="flex rounded-[10px] bg-base-profile drop-shadow-lg">
       <Image
         src={profile.avatarUrl}
         width={148}
@@ -22,10 +22,11 @@ async function ProfileBannerBase() {
         className="my-8 ml-10 h-fit rounded-lg"
       />
 
-      <section className="ml-8 mt-10 mb-8 flex w-full flex-col justify-between pr-8">
+      <section className="ml-8 mt-10 mb-8 mr-8 flex w-full flex-col justify-between">
         <div className="flex w-full flex-col">
-          <div className="flex justify-between">
-            <h1 className="text-2xl font-bold  text-base-title">{profile.name}</h1>
+          <div className="flex items-start justify-between gap-4">
+            <h1 className="text-2xl font-bold text-base-title">{profile.name}</h1>
+
             <a
               className="flex items-center gap-2 text-xs font-bold text-blue underline-offset-4 hover:underline"
               href={profile.url}
@@ -35,10 +36,10 @@ async function ProfileBannerBase() {
             </a>
           </div>
 
-          <p className="mt-2 leading-[160%]">{profile.bio}</p>
+          <p className="mt-2 break-words leading-[160%]">{profile.bio}</p>
         </div>
 
-        <footer className="mt-4 flex gap-6">
+        <footer className="mt-4 flex flex-col gap-2 sm:grid sm:grid-cols-2 md:flex md:flex-row md:gap-6">
           <span className="flex items-center gap-2 text-base-subtitle">
             <FontAwesomeIcon
               icon={faGithub}
