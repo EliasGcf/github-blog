187bff44af52e134495501d14fe67715a7d4079a
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-11-21 21:07:01 -0300

    chore: use twMerge and change border size

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,12 +1,16 @@
 import { InputHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type TextInputProps = InputHTMLAttributes<HTMLInputElement>;
 
-export function TextInput({ ...rest }: TextInputProps) {
+export function TextInput({ className, ...rest }: TextInputProps) {
   return (
     <input
       type="text"
-      className="h-[3.125rem] w-full rounded-md border border-base-border bg-base-input px-4 text-base text-base-text outline-none placeholder:text-base-label focus:border-blue"
+      className={twMerge(
+        'h-[3.125rem] w-full rounded-md border-2 border-base-border bg-base-input px-4 text-base text-base-text outline-none transition-colors placeholder:text-base-label focus:border-blue',
+        className
+      )}
       {...rest}
     />
   );
