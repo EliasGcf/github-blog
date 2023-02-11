22d02f07c7d949e91fd040cd35fe77a7692b0422
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-02-11 19:50:00 -0300

    chore: add loading to post page

---
+++ src/app/post/[number]/loading.tsx
@@ -0,0 +1,9 @@
+import { Loading } from '@components/Loading';
+
+export default function PostLoading() {
+  return (
+    <div className="flex items-center justify-center">
+      <Loading />
+    </div>
+  );
+}
